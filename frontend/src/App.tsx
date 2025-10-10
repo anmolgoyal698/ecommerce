e268daca60d32e4c7512adb134351c9adcc66cb3
anmolgoyal698 <anmolgoyal698@gmail.com>
2025-10-10 13:17:06 +0530

    Added initial configuration for using redux toolkit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,28 +2,30 @@ import HomeScreen from "./screens/HomeScreen";
 import RootLayout from "./components/RootLayout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ProductDetailsScreen from "./screens/ProductDetailsScreen";
-
+import store from "./store";
+import { Provider } from "react-redux";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <RootLayout/>,
+    path: "/",
+    element: <RootLayout />,
     children: [
       {
         index: true,
-        element: <HomeScreen/>
+        element: <HomeScreen />,
       },
       {
-        path: '/products/:id',
-        element: <ProductDetailsScreen/>
-      }
-    ]
-  }
+        path: "/products/:id",
+        element: <ProductDetailsScreen />,
+      },
+    ],
+  },
 ]);
 
-
 const App = () => (
-  <RouterProvider router={router}/>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
 
 export default App;
---
+++ frontend/src/slices/apiSlice.ts
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { BASE_URL } from "../constants";
+
+
+export const apiSlice = createApi({
+    baseQuery: fetchBaseQuery({baseUrl: BASE_URL}),
+    tagTypes: ['Product', 'User', 'Order'], // used to define the type of data that will be returned from APIs
+    endpoints: (builder) => ({
+        
+    })
+
+})
